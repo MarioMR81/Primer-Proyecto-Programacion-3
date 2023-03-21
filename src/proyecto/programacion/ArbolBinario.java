2a92de16dcb54ba12c2642796ff4d0a171ab4682
MarioMR81 <qmariomr81@gmail.com>
2023-03-20 21:24:19 -0600

    Metodos Notaciones

--- src/proyecto/programacion/ArbolBinario.java
+++ src/proyecto/programacion/ArbolBinario.java
@@ -33,3 +33,54 @@ public class ArbolBinario {
         raiz = pila.pop();
     }
 }
+
+    // verificar si es un operador
+    boolean esOperador(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/") || token.equals("^") || token.equals("sqrt");
+    }
+
+    public void imprimirNotacionPolaca() {
+        System.out.print("Notación Polaca o Preorden: ");
+        imprimirNotacionPolaca(raiz);
+        System.out.println();
+    }
+
+    private void imprimirNotacionPolaca(Nodo nodo) {
+        if (nodo != null) {
+            System.out.print(nodo.valor + " ");
+            imprimirNotacionPolaca(nodo.izquierdo);
+            imprimirNotacionPolaca(nodo.derecho);
+        }
+    }
+
+    public void imprimirInorder() {
+        System.out.print("Inorder: ");
+        imprimirInorder(raiz);
+        System.out.println();
+    }
+
+    private void imprimirInorder(Nodo nodo) {
+        if (nodo != null) {
+            imprimirInorder(nodo.izquierdo);
+            System.out.print(nodo.valor + " ");
+            imprimirInorder(nodo.derecho);
+        }
+    }
+
+    public void imprimirPostorden() {
+        System.out.print("Notación Postorden: ");
+        imprimirPostorden(this.raiz);
+        System.out.println();
+    }
+
+    private void imprimirPostorden(Nodo nodo) {
+        if (nodo != null) {
+            return;
+        } else {
+            this.imprimirPostorden(nodo.izquierdo);
+            this.imprimirPostorden(nodo.derecho);
+            System.out.print(nodo.valor + " ");
+
+        }
+
+    }
