2f44b94e16f5d4df331a2143714708e1429883e4
erycktzalam <erycktzalam@gmail.com>
2023-03-20 21:47:12 -0600

    Se agrega la clase nodo

--- src/proyecto/programacion/Nodo.java
+++ src/proyecto/programacion/Nodo.java
@@ -3,5 +3,13 @@ package proyecto.programacion;
 
 
 public class Nodo {
-    
+    String valor;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    Nodo(String valor) {
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
 }
