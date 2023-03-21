c399d94308b409ab4f2ed31a61d8ea5117b6280f
unknown <gmonzonf1@miumg.edu.gt>
2023-03-20 21:05:45 -0600

    se agrega metodo arbol binario

--- src/proyecto/programacion/ArbolBinario.java
+++ src/proyecto/programacion/ArbolBinario.java
@@ -3,5 +3,33 @@ package proyecto.programacion;
 
 
 public class ArbolBinario {
-    
+    Nodo raiz;
+
+    public ArbolBinario() {
+        this.raiz = null;
+    }
+
+    void crearArbol(String expresion) {
+        Stack<Nodo> pila = new Stack<Nodo>();
+        String[] tokens = expresion.split(" ");
+
+        for (String token : tokens) {
+            Nodo nodo = new Nodo(token);
+
+            if (esOperador(token)) {
+                nodo.derecho = pila.pop();
+                nodo.izquierdo = pila.pop();
+            } else if (token.equals(")")) {
+                while (!pila.peek().valor.equals("(")) {
+                    Nodo hijo = pila.pop();
+                    hijo.derecho = nodo.izquierdo;
+                    hijo.izquierdo = nodo.derecho;
+                    nodo = hijo;
+                }
+                pila.pop();
+            }
+            pila.push(nodo);
+        }
+        raiz = pila.pop();
+    }
 }
